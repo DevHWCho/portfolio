b7418eaeabf6e99e7d236cff7bfb6a133ca8aa89
DevHWCho <User address(whgusdnd97@gmail.com)>
2023-05-18 19:31:30 +0900

    fix

--- src/components/SamsungEm.js
+++ src/components/SamsungEm.js
@@ -56,7 +56,7 @@ function SamsungEm() {
         <div className='video_mockup'>
           <img src={require('../images/mockup/iMac.png')} alt=''></img>
           <div className='video_screen'>
-            <video autoPlay muted loop preload='true'>
+            <video autoPlay muted loop preload='true' playsinline>
               <source src={video}></source>
             </video>
           </div>
